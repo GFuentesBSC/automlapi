e51f03adabff5f2642d5eec278e1398e1427a3c4
GFuentesBSC <raul.garcia@bsc.es>
2020-10-07 11:57:49 +0200

    setup updated

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Raul Garcia',                   # Type in your name
   author_email = 'raulgfuentes97@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/GFuentesBSC/automlapi',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/GFuentesBSC/automlapi/archive/v0.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/GFuentesBSC/automlapi/archive/v0.2.tar.gz',    # I explain this later on
   keywords = ['AUTOML', 'BOTO3', 'AWS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'tqdm',
