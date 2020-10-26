1ad83044e2fda17f5d63217948e7cb19eec29b69
GFuentesBSC <raul.garcia@bsc.es>
2020-10-26 10:47:15 +0100

    v0.18

--- automlapi/__init__.py
+++ automlapi/__init__.py
@@ -4,5 +4,7 @@ from .automl_batch import *
 from .automl_cloudwatch import *
 from .automl_cognito import *
 from .automl_rds import *
+from .automl_ec2 import *
+from .automl_ecs import *
 from .automl_s3 import *
 from .automlClient import APIUser, private_upload_dataset, make_lst
