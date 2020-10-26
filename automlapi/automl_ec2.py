19f34cc0a7f25048419c13bde3820007b3b29202
GFuentesBSC <raul.garcia@bsc.es>
2020-10-26 23:31:04 +0100

    v0.27

--- automlapi/automl_ec2.py
+++ automlapi/automl_ec2.py
@@ -7,24 +7,24 @@ client_ec2 = boto3.client('ec2',
 						region_name='us-west-2')
 
 def launch_instances_for_flask(num_instances, predict=False):
-    num_instances = min(num_instances, 10)
-    num_instances = max(num_instances, 0)
+	num_instances = min(num_instances, 10)
+	num_instances = max(num_instances, 0)
 	if predict:
 		template = 'lt-056f7ab54b4e19392'
 	else:
 		template = 'lt-0c52b57123d230c97'
-    response = client_ec2.run_instances(
-        LaunchTemplate={
-            'LaunchTemplateId': template,
-            'Version': '1'
-        },
-        MaxCount=num_instances,
-        MinCount=num_instances
-    )
-    launched_instances_ids = []
-    launched_instances = response['Instances']
-    for launched_instance in launched_instances:
-        launched_instances_ids.append(launched_instance['InstanceId'])
-    print(f"launch_instances_for_flask : INFO : ids launched = {launched_instances_ids}")
+	response = client_ec2.run_instances(
+		LaunchTemplate={
+			'LaunchTemplateId': template,
+			'Version': '1'
+		},
+		MaxCount=num_instances,
+		MinCount=num_instances
+	)
+	launched_instances_ids = []
+	launched_instances = response['Instances']
+	for launched_instance in launched_instances:
+		launched_instances_ids.append(launched_instance['InstanceId'])
+	print(f"launch_instances_for_flask : INFO : ids launched = {launched_instances_ids}")
 
-    return launched_instances_ids
+	return launched_instances_ids
