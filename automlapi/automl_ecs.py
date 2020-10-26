5e880605b9c74bd20497140b8c77ea660dfdc733
GFuentesBSC <raul.garcia@bsc.es>
2020-10-26 10:49:34 +0100

    v0.19

--- automlapi/automl_ecs.py
+++ automlapi/automl_ecs.py
@@ -6,12 +6,12 @@ client_ecs = boto3.client('ecs',
 						aws_secret_access_key=AWS_SEC_ACC_KEY,
 						region_name='us-west-2')
 
-def update_flask_service_instances(num_instances):
+def update_flask_service_instances(service, num_instances):
     num_instances = min(num_instances, 10)
     num_instances = max(num_instances, 0)
     response = client_ecs.update_service(
         cluster='flask-cluster',
-        service='predict',
+        service=service,
         desiredCount=num_instances
     )
     return response
