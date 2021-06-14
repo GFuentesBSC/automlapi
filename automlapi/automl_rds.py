7a02813ee78930c39426ab19f93631fa62b2592a
GFuentesBSC <raul.garcia@bsc.es>
2021-06-14 19:39:15 +0200

    0.73

--- automlapi/automl_rds.py
+++ automlapi/automl_rds.py
@@ -235,9 +235,9 @@ def validate_user(account_code, username, password):
     hashed_password = hash_password(password)
     return validate_admin(account_code, username, hashed_password) or validate_projectManager(account_code, username, hashed_password)
 
-def insert_document(uploadDate, filename, extension, phase, uri, nPages, training, dataset_id, uploadMethod_id):
-	query = f'INSERT INTO neuralplatform_document(uploadDate, name, extension, phase, uri, nPages, training, dataset_id, uploadMethod_id) ' + \
-			f'VALUES ("{uploadDate}", "{filename}", "{extension}", "{phase}", "{uri}", {nPages}, {training}, {dataset_id}, {uploadMethod_id});'
+def insert_document(uploadDate, filename, extension, phase, uri, nPages, training, dataset_id, uploadMethod_id, project_id):
+	query = f'INSERT INTO neuralplatform_document(uploadDate, name, extension, phase, uri, nPages, training, dataset_id, uploadMethod_id, project_id) ' + \
+			f'VALUES ("{uploadDate}", "{filename}", "{extension}", "{phase}", "{uri}", {nPages}, {training}, {dataset_id}, {uploadMethod_id}, {project_id});'
 	return run_insert(query)
 
 def insert_request(phase, requestDate, document_id, project_id):
