3e17c50564b8c9587e7ff934f8632081eab15047
GFuentesBSC <raul.garcia@bsc.es>
2021-06-18 17:23:28 +0200

    v0.75

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'automlapi',
   packages = ['automlapi'],
-  version = '0.74',
+  version = '0.75',
   license='MIT',
   description = 'api for the AutoML project',
   author = 'Raul Garcia',
   author_email = 'raulgfuentes97@gmail.com',
   url = 'https://github.com/GFuentesBSC/automlapi',
-  download_url = 'https://github.com/GFuentesBSC/automlapi/archive/v0.74.tar.gz',
+  download_url = 'https://github.com/GFuentesBSC/automlapi/archive/v0.75.tar.gz',
   keywords = ['AUTOML', 'BOTO3', 'AWS'],   # Keywords that define your package best
   install_requires=[
           'tqdm',
