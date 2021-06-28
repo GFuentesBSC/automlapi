a585d3ecc9df04355d4ad5324f533e8f4408de95
GFuentesBSC <raul.garcia@bsc.es>
2021-06-28 16:44:56 +0200

    v0.84

--- automlapi/automl_rds.py
+++ automlapi/automl_rds.py
@@ -221,7 +221,7 @@ def get_bucketName_by_page_id(page_id):
         query = "SELECT s3Bucket FROM neuralplatform_account WHERE id = " + \
             "(SELECT account_id FROM neuralplatform_project WHERE id = " + \
             "(SELECT project_id FROM neuralplatform_document WHERE id = " + \
-            f"(SELECT document_id FROM neuralplatform_page WHERE id = {page_id}))));"
+            f"(SELECT document_id FROM neuralplatform_page WHERE id = {page_id})));"
         return run_select(query)[0]['s3Bucket']
     except Exception as e:
         print(f"get_bucketName_by_page_id : Error : {e}")
--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'automlapi',
   packages = ['automlapi'],
-  version = '0.83',
+  version = '0.84',
   license='MIT',
   description = 'api for the AutoML project',
   author = 'Raul Garcia',
   author_email = 'raulgfuentes97@gmail.com',
   url = 'https://github.com/GFuentesBSC/automlapi',
-  download_url = 'https://github.com/GFuentesBSC/automlapi/archive/v0.83.tar.gz',
+  download_url = 'https://github.com/GFuentesBSC/automlapi/archive/v0.84.tar.gz',
   keywords = ['AUTOML', 'BOTO3', 'AWS'],   # Keywords that define your package best
   install_requires=[
           'tqdm',
