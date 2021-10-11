b72784ed73663ba773632d70d722417e9a3dc606
GFuentesBSC <raul.garcia@bsc.es>
2021-10-11 18:00:37 +0200

    v0.123

--- automlapi/automl_ecs.py
+++ automlapi/automl_ecs.py
@@ -2,6 +2,9 @@ import boto3
 from .automl import AWS_ACC_KEY_ID, AWS_SEC_ACC_KEY, AWS_REGION_NAME
 import time
 from botocore.exceptions import ClientError
+from .automl_elb import (
+	get_targets_health,
+)
 
 client_ecs = boto3.client('ecs',
 						aws_access_key_id=AWS_ACC_KEY_ID,
@@ -92,22 +95,6 @@ def run_model_deployment(version_id, model_uri):
 		)
 	return response['ResponseMetadata']['HTTPStatusCode']
 
-### DEPRECATED ######
-def update_flask_service_instances(service, num_instances, cluster_name):
-	num_instances = min(num_instances, 20)
-	num_instances = max(num_instances, 0)
-	print(f"update_flask_service_instances : INFO : Requesting {num_instances} for service {service}...")
-	try:
-		response = client_ecs.update_service(
-			cluster=cluster_name,
-			service=service,
-			desiredCount=int(num_instances),
-			forceNewDeployment=True
-		)
-	except Exception:
-		return False
-	return True
-
 def get_service_instaces_status(cluster, service_name):
 	print(f"get_service_instaces_status : INFO : Getting info of service {service_name} (cluster {cluster})...")
 	try:
@@ -119,89 +106,11 @@ def get_service_instaces_status(cluster, service_name):
 	except:
 		return 0, 0
 
-def services_ready(cluster, service_list):
-	response = client_ecs.describe_services(cluster=cluster, services=service_list)
-	services = response['services']
-	for service in services:
-		name    = service['serviceName']
-		desired = service['desiredCount']
-		running = service['runningCount']
-		print(f"services_ready : INFO : Service {name}: Desired = {desired}, Running = {running}")
-		if desired != running:
-			return False
-	return True
-
-def create_cluster(cluster_name):
-	response = client_ecs.create_cluster(clusterName=cluster_name)
-
-def create_task_definition(task_definition_name, ecr_image):
-	response = client_ecs.register_task_definition(
-	    family=task_definition_name,
-	    taskRoleArn='arn:aws:iam::749868801319:role/ecsTaskExecutionRole',
-	    executionRoleArn='arn:aws:iam::749868801319:role/ecsTaskExecutionRole',
-	    networkMode='bridge',
-	    containerDefinitions=[
-	        {
-	            'name': 'CONTAINER',
-	            'image': ecr_image,
-	            'cpu': 0,
-	            'memory': 6144,
-	            'portMappings': [
-	                {
-	                    'containerPort': 5000,
-	                    'hostPort': 5000,
-	                    'protocol': 'tcp'
-	                },
-					{
-	                    'containerPort': 5555,
-	                    'hostPort': 5555,
-	                    'protocol': 'tcp'
-	                }
-	            ],
-	            'essential': True,
-	            'workingDirectory': '/'
-	        },
-	    ],
-	    requiresCompatibilities=[
-	        'EC2',
-	    ],
-	    cpu='2000',
-	    memory='6144'
-	)
-	if int(response['ResponseMetadata']['HTTPStatusCode']) == 200:
-		print(f'create_task_definition : INFO : Created TaskDefinition {task_definition_name}!')
-		return str(response['taskDefinition']['taskDefinitionArn'])
-	else:
-		print(f'create_task_definition : ERROR : Could not create TaskDefinition {task_definition_name}!')
-		return False
-
-def create_service(cluster, serviceName, taskDefinition, targetGroupArn):
-	response = client_ecs.create_service(
-	    cluster=cluster,
-	    serviceName=serviceName,
-	    taskDefinition=taskDefinition,
-	    loadBalancers=[
-	        {
-	            'targetGroupArn': targetGroupArn,
-	            'containerName': 'CONTAINER',
-	            'containerPort': 5000
-	        },
-	    ],
-	    desiredCount=0,
-	    launchType='EC2',
-	    role='arn:aws:iam::749868801319:role/ecsServiceRole',
-	    deploymentConfiguration={
-	        'maximumPercent': 200,
-	        'minimumHealthyPercent': 0
-	    },
-	    placementStrategy=[
-	        {
-	            'type': 'binpack',
-	            'field': 'memory'
-	        },
-	    ],
-	    healthCheckGracePeriodSeconds=2147483646,
-	    schedulingStrategy='REPLICA',
-	    enableECSManagedTags=True
-	)
-	return int(response['ResponseMetadata']['HTTPStatusCode']) == 200
+def get_draining_instances_count(cluster_name, service_name):
+	try:
+		tg_arn = client_ecs.describe_services(cluster=cluster_name, services=[service_name])['services'][0]['loadBalancers'][0]['targetGroupArn']
+		targets_health = get_targets_health(tg_arn)
+		return len(list(filter(lambda x: x.lower() == 'draining', targets_health)))
+	except Exception as e:
+		print(f"get_draining_instances_count : ERROR : {e}")
+		return 0
