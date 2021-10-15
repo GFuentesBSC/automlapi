0c83adc23628944e6a52272a83baa8600b844da4
GFuentesBSC <raul.garcia@bsc.es>
2021-10-15 18:23:33 +0200

    automl_eb

--- automlapi/__init__.py
+++ automlapi/__init__.py
@@ -5,6 +5,7 @@ from .automl_cloudwatch import *
 from .automl_cognito import *
 from .automl_rds import *
 from .automl_codebuild import *
+from .automl_eb import *
 from .automl_ec2 import *
 from .automl_elb import *
 from .automl_ecs import *
